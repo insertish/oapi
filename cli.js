c5f52828a92d15fa4dd16ea537155ad48abd7b62
William Horning <wgyt735yt@gmail.com>
2023-08-15 20:28:58 -0400

    remove semicolon that shouldn't be there

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-":"; //# comment; exec /usr/bin/env node --input-type=module - "$@" < "$0"
+":" //# comment; exec /usr/bin/env node --input-type=module - "$@" < "$0"
 
 import { readFile, writeFile } from "fs/promises";
 import openapi from "openapi-typescript";
